4703d96df276e461bd907b9c0fcebbac9af20c3b
Nimesh Johari <nimeshjohari95@gmail.com>
2020-10-10 14:53:41 +0530

    Added Result Route With Css

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
+@app.route('/result')
+def result():
+    return render_template('Result.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
